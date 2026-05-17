9059749bf410ce188c11b9d3b4a89c68cf679d20
faketaxi873861-dev <faketaxi873861@gmail.com>
2026-05-18 01:13:08 +0530

    Update app.py

--- TelegramFramework/app.py
+++ TelegramFramework/app.py
@@ -3,7 +3,7 @@ import asyncio
 import pandas as pd
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from telethon.tl.types import DocumentAttributeFilename
+from telethon.tl.types import DocumentAttributeFilename, MessageMediaDocument
 import pytz
 import qrcode
 from io import BytesIO
@@ -34,7 +34,7 @@ st.markdown("""
 
 # --- UI HEADER ---
 st.title("📡 Advanced Telegram Message Scraper")
-st.markdown(f"📊 *Enhanced Hybrid Engine (Separated Targets & Auto-Discovery)* | **Made by {NAME}**")
+st.markdown(f"📊 *Enhanced Hybrid Engine (Asset Matcher & Content Classifier)* | **Made by {NAME}**")
 st.divider()
 
 # --- ASYNC LOOP HANDLING ---
@@ -50,6 +50,125 @@ if "client" not in st.session_state:
     loop.run_until_complete(st.session_state.client.connect())
 
 client = st.session_state.client
+
+# --- MASTER ASSET LIST ---
+ASSET_LIST = [
+    "Akbar Birbal", "Ramayan Sabke Jeevan Ka Aadhar", "Al Akhawat (Maayka) (Zee Alwan)", 
+    "Al Malik W Al Malika (Ek Tha Raja Ek Thi Rani)", "Anta Mahboobi (Rabb Se Hai Dua)", 
+    "Jodha Akbar (Arabic)", "Jodha Akbar(Indonesia)", "Jodha And Akbar", "Les Voeux De Vidya", 
+    "Pavithra Rishta", "Qubool", "Radha Mohan (Pyar Ka Pehla Naam Radha Mohan)", "Siddhivinayak", 
+    "Zindagi Ki Mehek (Zee Alwan)", "Bhagya Lakshmi (Arabic)", "Kumkum Bhagya (Arabic)", 
+    "Kumkum Bhagya (USA)", "Kundali Bhagya (Arabic)", "My Journey To You (Kaise Mujhe Tum Mil Gaye)", 
+    "Aap Ke Aa Jane Se", "Aga Aga Sunbai, Kay Mhanta Sasubai?", "Agar Tum Na Hote", 
+    "Aisi Deewangi Dekhi Nahi Kahi", "Apna Time Bhi Aayega", "Bandbudh Aur Budbak", 
+    "Bhootu Hindi", "Brahmarakshas", "Choti Bahu", "Daayan", "Dadagiri", "Dance Bangla Dance (All seasons)", 
+    "Do Dil Bandhe Ek Dori Se", "Ek Mahanayak - Dr B R Ambedkar", "Ek Mutthi Aasmaan", 
+    "Ek Vivah Aisa Bhi", "Fear Files", "Guddan Tumse Na Ho Paayega", "Ikk Kudi Punjab Di", 
+    "Iniya Iru Malargal", "Jamai Raja", "Jodha Akbar", "Laal Ishq (Hindi)", "Lag Ja Gale", 
+    "Lakshmi Raave Maa Intiki", "Main Hoon Sath Tere", "Mr. Nonsense", "Qubool Hai", 
+    "Sa Re Ga Ma Pa Lil Champs (Tamil)", "Saat Paake Bandha", "Sanjog", "Thirumangalyam", 
+    "Vishkanya (Hindi)", "Woh Apna Sa", "Yeh Teri Galiyan", "Zee Rishtey Awards (All Years)", 
+    "Zindagi Ki Mehek", "Suryakantham", "Kaise Mujhe Tum Mil Gaye", "Rannaghar", 
+    "Savalyachi Janu Savali", "Ammayi Garu", "Amruthadhaare", "Anna", "Atal", "Bhabi Ji Ghar Par Hai!", 
+    "Bhagya Lakshmi", "CHIRANJEEVI LAKSHMI SOWBHAGYAVATI", "Didi No.1 (Bengali)", "Gundamma Katha", 
+    "Happu Ki Ultan Paltan", "Idhayam", "Jabilli Kosam Aakashamalle", "Jagadhatri (Telugu)", 
+    "Janaki Ramayya Gari Manavaralu", "Kalavaari Kodalu Kanakamahalakshmi", "Karthigai Deepam", 
+    "Kon Gopone Mon Bheseche", "Kumkum Bhagya", "Kundali Bhagya", "Lakshmi Kalyanam", "Maa Annayya", 
+    "Maari", "Mala Bodol", "Meghasandesam", "Mukkupudaka", "Neem Phooler Madhu", "Ninaithale Inikkum", 
+    "Olimayamana Ethirkaalam", "Omkaram", "Padamati Sandhyaragam", "Prema Entha Maduram", 
+    "Puber Moyna", "Rabb Se Hai Dua", "Sa Re Ga Ma Pa (Bangla)", "Sandhya Raagam", "Seetha Rama", 
+    "Seethe Ramudi Katnam", "Tamizha Tamizha S3", "Trinayani (Telugu)", "Vasudha", "Veera", 
+    "Nindu Noorella Saavasam", "Bahu Beti Aur ZEE5", "Anondi", "Jagriti", "SaReGaMaPa 2024", 
+    "Jaane Anjaane Hum Mile", "Mounam Pesiyadhe", "Parineeta", "Jamai No. 1", "Lakshmi Niwas", 
+    "Chamanthi", "Manasellam", "Gattimelam", "Naa Ninna Bidalaare", "Dance Bangla Dance S13", 
+    "Duggamoni O Bagh Mama", "Chirodini Tumi Je Amar", "Tui Amar Hero (Tv Show)", "A Aa", 
+    "Antim The Final Truth", "Attack: Part 1", "Babumoshai Bandookbaaz (Hindi)", "Bal Naren", 
+    "Bangarraju Hindi Dubbed", "Bareilly Ki Barfi", "Baster – The Naxal Story", "Bengal Tiger (Hindi)", 
+    "Bhaiyya Ji", "Bhaje Vaayu Vegam", "Chal Mohan Ranga", "Commando 3 (Hindi)", "DD Returns", 
+    "Deool Band", "Dhaakad (Hindi)", "Dharmaveer 2: Mukkam Post Thane", "Dharmaveer: Mukkam Post Thane", 
+    "Farrey", "Gadar 2 – The Katha Continues", "Gadar: Ek Prem Katha (Hindi)", "Game Changer", 
+    "Gangubai Kathiawadi", "Geeta Govinda", "Ghoomer", "Hacked (Hindi)", "Hotel Mumbai (Hindi)", 
+    "Identity", "Interrogation", "iSmart Shankar", "Janhit mein Jaari", "Jersey (Hindi)", 
+    "Kaho Naa... Pyaar Hai (Hindi)", "Kala Shah Kala", "Kalavani 2", "Kanaa (Tamil Movie)", 
+    "Kanam", "Kanchana 3", "Kathar Basha Endra Muthuramalingam", "Kavan", "Kayal", "Kedarnath", 
+    "Kennedy Club", "Khuda Haafiz Chapter 2: Agni Pariksha", "Kisi Ka Bhai Kisi Ki Jaan", 
+    "Kolamavu Kokila(KOKO)", "Krack", "Max", "Mulshi Pattern (Marathi)", "Padman", 
+    "Qarib Qarib Singlle (Hindi)", "Raksha Bandhan (Hindi)", "Rashtra Kavach Om - The Battle Within", 
+    "Real Tevar", "Sam Bahadur", "Sankranthiki Vasthunam", "Sarla Ek Koti", "Simmba", "Srimanthudu", 
+    "Surya The Soldier", "Tejas", "The Kashmir Files (Hindi)", "The Kerala Story", "The Sabarmati Report", 
+    "Uri The Surgical Strike (Hindi)", "Valimai (Hindi)", "Valimai (Telugu)", "Vedaa", 
+    "Viduthalai - Part 1", "Woh Bhi Din The", "Kudumbasthan", "Abar Proloy", "Abhay", 
+    "Ayali (Web Series)", "Churails", "Dhoop Ki Deewar", "Duranga", "Gyaarah Gyaarah", 
+    "Jaanbaaz Hindustan ke", "Kaafir", "Manorathangal", "Rangbaaz: Darr Ki Rajneeti", "Sunflower", 
+    "Taj", "The Broken News", "The Kashmir Files: Unreported", "Berlin", "Cabaret", "Chhatriwali", 
+    "Kadak Singh", "Kakuda", "Kanjoos Makhichoos", "Mrs Undercover", "Rautu Ka Raaz", 
+    "Silence 2: The Night Owl Bar Shootout", "Sirf Ek Bandaa Kaafi Hai", "State of Siege: Temple Attack", 
+    "Tarla", "The Signature", "U-Turn", "Murshid", "Aindham Vedham", "Mithya", 
+    "Divorce Ke Liye Kuch Bhi Karega", "Despatch", "Khoj - Parchaiyo Ke Uss Paar", "Hisaab Barabar", 
+    "Mrs", "Pyaar Testing", "Crime Beat", "Vanvaas", "Viduthalai Part 2", "Prem Pratigya", "Parul", 
+    "Kaafir (Movie)", "Tum Se Tum Tak", "Detective Sherdil", "Logout", "Parvati – Umeed Zindagi Ki", 
+    "Ayyana Mane", "Costao", "Robin Hood 2025", "Ata Thambaycha Naay!", "Saru", "Andhar Maya", 
+    "SaReGaMaPa Seniors Season 05", "Sang Hoon Main Tere", "Ayali (TV Shows)", "Uma Kaali", 
+    "Chhal Kapat - The Deception", "Kusum", "Bibhishon", "Vikram (2022)", "Varisu", "Kamali (Marathi)", 
+    "Kaalidhar Laapata", "Jarann", "Amader Dadamoni", "The Bhootnii", "Bhairavam", "Meghasandhesham", 
+    "Samayal Express S2", "Sattamum Needhiyum", "Saunkan Saunkanay 2", "Chinnan Siru Kiliye", 
+    "Main Teri Parchayi", "Bahu No. 1", "Bakaiti", "Tehran", "Aamar Boss", "Chhoriyan Chali Gaon", 
+    "JSK-Janaki V Vs State of Kerala", "Single Pasanga", "Aankhon ki Gustakhiyaan", 
+    "Veen Doghatli Hi Tutena", "Taarini", "Jayammu NischayammuRaa with Jagapathi", "Salangai Oli", 
+    "Bedeni Jyotsnar Amar Prem", "Dosh Dine Dosh Lakh", "Kone Dekhaa Alo", "Dakuaan Da Munda 3", 
+    "Kahani Har Ghar Ki", "Sri Raghavendra Mahatme", "Saregamapa 2025", "Paarijatham", "Jowar Bhanta", 
+    "Housemates", "Janaawar - The Beast Within", "Mrigaya: The Hunt", "Raja Chinna Roja", 
+    "Ganga Mai Ki Betiyan", "SUMATHI VALAVU", "Bhagwat Chapter One - Raakshas", "Kishkindhapuri", 
+    "Madam Sengupta", "Ideabaaz", "Dashavatar", "Thode Door Thode Paas", "Rangbaaz: The Bihar Chapter", 
+    "Ganoshotru", "Thirumangalyam New", "The Bengal files", "Jagadhatri (Hindi)", "Raktabeej 2", 
+    "Honeymoon Se Hatya", "The pet detective", "Regai", "Aval Varuvala", "Annamalai Kudumbam", 
+    "Sach - Subhash Chandra Show", "Saali Mohabbat", "Shabad - Reet aur Riwaaz", "Gharwali Pedwali", 
+    "Ronkini Bhavan", "Heartiley Battery", "Besh Korechi Prem Korechi", "SaReGaMaPa Lil Champs Season 5", 
+    "Devi Chowdhurani: Bandit Queen of Bengal", "Godday Godday Chaa 2", "Taare Dhori Dhori Mone Kori", 
+    "Ek Diwaane Ki Deewaniyat", "Date with Saie (Movie)", "Bhabiji Ghar Par Hai 2.0", "Vritta", 
+    "Joto Kando Kolkatatei", "45", "Lakshmi Nivas", "Mastiii 4", "Sirai", "Safia Safdar", 
+    "Shubh Shravanii", "Kaalipotka", "Vaagai Sooda Vaa", "PARASAKTHI", "Devkhel", 
+    "Bhanupriya Bhooter Hotel", "Mana Shankara Vara Prasad Garu", "Uttar", "Kennedy", 
+    "Krantijyoti Vidyalay Marathi Madhyam", "Shabdam", "Jab Khuli Kitaab", "Thadayam", 
+    "Bhartha Mahasayulaku Wignyapthi", "Landlord", "Abar Proloy 2", "Gandhi Talks", 
+    "ZEE CINE AWARDS 2026", "Pookie", "Andha Pyaar 2.0", "PRAKAMBANAM", 
+    "Aga Aga Saunbai Kay Mhantay Sasubai (Movie)", "Street 9 (Sharea Tessa)", "Saat Pake Bandha", 
+    "Kasaragod Embassy", "Abdullahpur Ka Devdas", "Bhabiji Ghar Par Hain: Fun on the Run", 
+    "Projapati 2", "Mom Talks: Maa Ki Baat Maa Ke Jazbaat", "Hey Balwanth", "Rubaab", 
+    "Everyone loves Saurabh Handa", "Kaakee Circus", "Kamala Nibas", "Kaathal the core", "Assi", 
+    "Ashakal Aayiram", "BANDMELAM", "JERAX", "Kerala Story 2", "Dhumketu", 
+    "Gaddi Jaandi Ae Chalaangaan Maardi", "Carry On Jatta 3", "Jatt & Juliet 3", "Kali Jotta", 
+    "Oye Bhole Oye", "Annhi Dea Mazaak Ae", "Aaja Mexico Challiye", "Blackia 2", "Je Jatt Vigarh Gya", 
+    "Ni Main Sass Kuttni 2", "Kudi Haryane Val Di", "Shinda Shinda No Papa", "Shikaari", 
+    "Jatt Nuu Chudail Takri", "Sarpanchi 2024", "Ucha Dar Babe Nanak Da", "Shayar", "Buhe Bariyan", 
+    "Laxmi Raj", "Rickshaw Romeo", "Teen Yaar Twisted Pyaar", "The Truth Serum Wife", "Two Sisters", 
+    "Muka Ghya Muka", "Pandu Hawaldar", "Songadya", "Tumcha Aamcha Jamla (Movie)", "Trademark of ZEE TV", 
+    "Annapoorna", "Brahmagantu", "Jagruti", "Kalyanamasthu", "Paaru (Marathi)", "Sandhyaraaga", 
+    "Shravani Subramanya", "Srikaram Shubhakaram", "Sa Re Ga Ma Pa - The Next Singing Youth Icon", 
+    "SA RE GA MA PA Lil Champs Season 4", "Ummadi Kutumbam", "Naag Bhairavi", "Ennallo Vechina Hrudayam", 
+    "Sathi Sata Janmara", "Tula Japnar Aahe", "Dance Jodi Dance Reloaded 3", "Super Serial Championship Season 4", 
+    "Bhaiaji Superhittt", "Fatteshikast (Marathi)", "Koi... Mil Gaya", "Parmanu The Story Of Pokhran", 
+    "Spyder (Hindi)", "Taxiwaala", "Riwaj", "Mazaka", "Kingston", "Daveed", 
+    "Tere Chehre Se Nazar Nahi Hatati", "Gharana Mogudu", "Agnyathavasi", "DD Next level", 
+    "Prince and Family", "Mafia - Game for Life", "Tamilarasan", "Sonar Jalsaghar", "Sriman Bhogoban Das", 
+    "S.I.T. Bengal", "Lights, Camera, Achhan", "AABHYANTHARA KUTTAVAALI", "What a Kismat", "Sthal", 
+    "Baai Tujhyapayi", "Comedy Khiladigalu 5", "Akshardham: Operation Vajra Shakti (2025)", 
+    "Vikkatakavi - The Movie", "Dance Karnataka Dance 2025", "Durga", "Murugesan +2", 
+    "Ondu Sarala Prema Kathe", "Be dune teen", "Paavanaganga", "Dance Odisha Dance - Celebrity with Little Masters", 
+    "Kesariya@100", "Court Martial", "Adi Lakshmi Purana", "Aranyar Prachin Probad", "Natsamrat", 
+    "Chembarathy", "Hamlet", "Lakshmi Raave Maa Intiki (New)", "Fab5", "Chote Tara ka Bada Gadar", 
+    "Aaranyak", "Beauty", "Mask", "Char Chaughi", "Kapus Kondyachi Goshta", "Bha Bha Ba", 
+    "Gurram Paapi Reddy", "Comedy Khiladi Juniors", "Parashakthi Pongal Kondattam", "Short & Sweet", 
+    "Khiladi Jodies", "Chi Sou Mahalakshmi", "Kongu Bangaram", "Jai 2026", "Aata", 
+    "NELLIKKAMPOYIL NIGHT RIDERS", "Drama Juniors Season 2", "Baal Krishna", "D/o Prasad Rao: Kanabadutaledhu", 
+    "Krishna Rukku", "Sose Tanda Soubhagya", "Kichi Sata Kichi Michha", "Alam Tani", 
+    "Noqta Al Ameya (Blind Spot)", "Zee Natya Gaurav Puraskar 2026", "Raja Jotaka", "Sanai Chaughade", 
+    "Ugadi Ki Vasthunam Sandadi Testhunam", "Bookie", "Hey Kay Navin?", "Karna (Telugu)", 
+    "The B Gang- Punjab Chapter", "Unlimit Awards 2026", "Deergha Sumangali Bhava", "Know Your Leader", 
+    "Raja Queen 2026", "Deep Jyoti", "DETECTIVE DHANANJAY: RAHASYAJAAL", "LIK Audio Launch", 
+    "Sa Re Ga Ma Pa Li'l Champs", "The Family Assembly", "Valyettan", "Boishakhi Hullor", 
+    "AADU 3", "Love Mocktail 3", "Bowling-ah? Fielding-ah?", "Cricket Darbar", "Tighee", "Neelira"
+]
 
 # --- SIDEBAR AUTHENTICATION ---
 with st.sidebar:
@@ -121,7 +240,6 @@ with st.sidebar:
 # --- SCRAPER UI ---
 st.sidebar.header("⚙️ Target Profile Configuration")
 
-# SEPARATED INPUTS: Box 1 for Identifiers/IDs, Box 2 for Private Joinchat links
 channel_target = st.sidebar.text_input(
     "Channel Identifier / ID", 
     value="",
@@ -150,7 +268,6 @@ async def fetch_joined_channels():
     return pd.DataFrame(channels_list)
 
 async def scrape_logic():
-    # Determine which target to use
     if joinchat_target.strip():
         target = joinchat_target.strip()
     else:
@@ -184,6 +301,24 @@ async def scrape_logic():
         if msg.date:
             dt = msg.date.astimezone(pytz.timezone(target_timezone)).strftime('%Y-%m-%d %H:%M:%S')
         
+        # --- FIXED: Content Type & Asset Matcher Logic ---
+        msg_text = msg.text or ""
+        
+        # 1. Classify Content Type (Video vs Message)
+        content_type = "Message"
+        if msg.video or (msg.media and isinstance(msg.media, MessageMediaDocument) and any(
+            getattr(attr, 'mime_type', '').startswith('video/') for attr in [msg.media.document] if hasattr(msg.media, 'document')
+        )):
+            content_type = "Video"
+            
+        # 2. Match Asset Name inside text or filename
+        matched_asset = "N/A"
+        search_blob = f"{msg_text} {fname}".lower()
+        for asset in ASSET_LIST:
+            if asset.lower() in search_blob:
+                matched_asset = asset
+                break  # Stops at first matching find
+        
         data.append({
             'Channel Name': channel_name,
             'Channel ID': entity.id,
@@ -191,7 +326,9 @@ async def scrape_logic():
             'Date': dt, 
             'Views': msg.views or 0, 
             'File Name': fname, 
-            'Message Content': msg.text or "",
+            'Asset Name': matched_asset,
+            'Content Type': content_type,
+            'Message Content': msg_text,
             'Message URL': f"{channel_url}/{msg.id}"
         })
         
@@ -210,7 +347,6 @@ if st.button("🚀 Execute Cloud Extraction"):
     if not is_authorized:
         st.error("Access Denied: Please authorize your session via the control sidebar panel first.")
         
-    # CONDITION: If both input boxes are empty, fetch and show channel list
     elif not channel_target.strip() and not joinchat_target.strip():
         st.info("🔍 Targets are empty! Fetching your joined channels list...")
         with st.spinner("Loading channels ledger from Telegram..."):
